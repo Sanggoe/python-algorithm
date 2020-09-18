9bdc175ca4f9be213a2dd87dac14bc0abb51453f
sanggoe <sanggoe0509@gmail>
2020-09-18 12:21:16 +0900

    algorithm

--- programmers/test.py
+++ programmers/test.py
@@ -1,5 +1,10 @@
-def solution(s):
-    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
+def solution(numbers):
+    a = list()
 
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            a.append(numbers[i] + numbers[j])
 
-print(solution("try hello world"))
+    return list(set(a))
+
+print(solution([5,0,2,7]))
