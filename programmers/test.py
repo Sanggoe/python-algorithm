5af2c25df6177d031319cd5fcd8e30d01571c677
sanggoe <sanggoe0509@gmail.com>
2020-11-06 12:24:54 +0900

    study algorithm

--- programmers/test.py
+++ programmers/test.py
@@ -1,7 +1,16 @@
-def solution(array, commands):
-    answer = []
+def solution(board, moves):
+    # stack 배열 초기화
+    stack_board = []
+    for _ in range(len(board)):
+        stack_board.append(list())
 
-    for c in commands:
-        answer.append(sorted(array[c[0] - 1:c[1]])[c[2]-1])
+    # stack 배열에 board 값 넣기
+    for line in board[::-1]:
+        for i in range(len(board)):
+            if line[i]:
+                stack_board[i].append(line[i])
 
-    return answer
+    print(stack_board)
+
+
+solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])
