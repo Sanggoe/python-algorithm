2a785672c57f2260cda88828ccc17585c05f7e35
sanggoe <sanggoe0509@gmail.com>
2020-11-13 12:09:44 +0900

    algorithm study

--- programmers/test.py
+++ programmers/test.py
@@ -1,16 +1,11 @@
-def solution(board, moves):
-    # stack 배열 초기화
-    stack_board = []
-    for _ in range(len(board)):
-        stack_board.append(list())
-
-    # stack 배열에 board 값 넣기
-    for line in board[::-1]:
-        for i in range(len(board)):
-            if line[i]:
-                stack_board[i].append(line[i])
-
-    print(stack_board)
+# 무식하게 짜는 방법 1
+def solution(n, m):
+    min, max = sorted([n, m])
+    while 1:
+        remain = max % min
+        if not remain:
+            return [min, n*m//min]
+        max, min = min, remain
 
 
-solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])
+print(solution(5, 2))
