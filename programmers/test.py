a6cfc82ae6a30b5a7d429a3a5085b1f34b8f2651
sanggoe <sanggoe0509@gmail.com>
2021-01-20 09:48:51 +0900

    study algorithm

--- programmers/test.py
+++ programmers/test.py
@@ -1,9 +1,50 @@
-def solution(arr1, arr2):
+'''
+10, [9,4,4,4,7], [2,7,8,10,6]
+[1,2,3,5,7]
 
-    for i in range(len(arr1)):
-        for j in range(len(arr1[0])):
-            arr1[i][j] += arr2[i][j]
+2 9
+4 6 7 8 10
+1
+3
+5
 
-    return []
 
-print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
+
+1 3 4 5
+2 10
+6 7
+8
+9
+
+2 3 6 8 9
+'''
+
+def solution(n, t1, t2):
+    answer = list(set([i for i in range(1, n+1)]) - set(t1 + t2))
+    group = []
+
+    for j in range(len(t1)):
+        if t1[j] in group or t2[j] in group:
+            continue
+
+        group = [t1[j], t2[j]]
+
+        for i in range(len(t1)):
+            if t1[i] in group:
+                if t2[i] not in group:
+                    group.append(t2[i])
+            if t2[i] in group:
+                if t1[i] not in group:
+                    group.append(t1[i])
+
+        group.sort()
+
+        if len(group) % 2 == 0:
+            answer.append(group[(len(group) // 2) - 1])
+        else:
+            answer.append(group[len(group) // 2])
+
+    return sorted(list(set(answer)))
+
+
+print(solution(10, [1,3,3,2,7], [4,4,5,10,6]))
