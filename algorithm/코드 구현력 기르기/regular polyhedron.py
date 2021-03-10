cc83b876cef33d6b4347b29179e7c52447b61d43
sanggoe <sanggoe0509@gmail.com>
2021-03-10 19:34:37 +0900

    first HackerRank

--- algorithm/코드 구현력 기르기/regular polyhedron.py
+++ algorithm/코드 구현력 기르기/regular polyhedron.py
@@ -22,6 +22,27 @@ for index, count in enumerate(cnt):
         print(index + 1, end=" ")
 
 
+
+# 내가 짠 코드 2회차
+import sys
+
+sys.stdin = open("input.txt", "r")
+N, M = map(int, input().split())
+nums = sorted([i + j for i in range(1, N+1) for j in range(1, M+1)])
+max = 0
+res = list()
+for n in set(nums):
+    count = nums.count(n)
+    if max < count:
+        res = [n]
+        max = count
+    elif max == count:
+        res.append(n)
+
+for n in sorted(res):
+    print(n, end=" ")
+
+
 # 강의 중 강사님 코드
 import sys
 sys.stdin = open("input.txt", "r")
--- algorithm/코드 구현력 기르기/section2_python_test.py
+++ algorithm/코드 구현력 기르기/section2_python_test.py
@@ -2,16 +2,17 @@
 import sys
 
 sys.stdin = open("input.txt", "r")
-N = int(input())
-nums = list(map(int, input().split()))
-avg = int(sum(nums) / len(nums) + 0.5)
-min = 1
+N, M = map(int, input().split())
+nums = sorted([i + j for i in range(1, N+1) for j in range(1, M+1)])
+max = 0
+res = list()
+for n in set(nums):
+    count = nums.count(n)
+    if max < count:
+        res = [n]
+        max = count
+    elif max == count:
+        res.append(n)
 
-for i, v in enumerate(nums):
-    if abs(avg - v) < abs(avg - nums[min]):
-        min = i
-    elif abs(avg - v) == abs(avg - nums[min]):
-        if v > nums[min]:
-            min = i
-
-print(avg, min+1)
+for n in sorted(res):
+    print(n, end=" ")
